569ec72bd364da165c905c438776f316e76d9194
Bodiwho <44253549+Bodiwho@users.noreply.github.com>
2020-02-25 09:11:51 +0100

    final final

--- js/button.js
+++ js/button.js
@@ -1,6 +1,6 @@
-var arr = ["tops/books.html",
+var arr = ["tops/people.html",
 , 
-"tops/food.html"];
+"tops/food.html","otros/about.html","otros/contact.html"];
 
 function randSite(){
         location.href = "" + arr[Math.floor (Math.random()*arr.length)];
